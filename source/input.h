d7d57136213a675af4ab44d22e28fbe0fef22af9
Blake Burgess <trinitek@live.com>
2014-12-14 05:56:20 -0500

    Add keyboard scancode definitions for special keys

--- source/input.h
+++ source/input.h
@@ -1,3 +1,10 @@
 
+#define KEY_UP      72
+#define KEY_DOWN    80
+#define KEY_LEFT    75
+#define KEY_RIGHT   77
+#define KEY_ESC     27
+#define KEY_ENTER   13
+
 char getKey(void);
 void waitForKey(void);
