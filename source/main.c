0c40f921b9aed2bdcb3af246a0faa6293fbed159
Blake Burgess <trinitek@live.com>
2014-12-14 17:05:59 -0500

    Cleanup testing code in main

--- source/main.c
+++ source/main.c
@@ -5,27 +5,44 @@ asm("org 32768");
 #include "screen.h"
 
 void main(void) {
+    // Set the video mode to VGA 256-color graphics
     setMode(0x13);
     
-    short x = 0;
-    short y = 0;
-    
-    putPixel(0, 0, 15);
-    putPixel(320-1, 200-1, 15);
+    // Put pixels at all four corners of the screen
+    putPixel(0, 0, 15);         // top left
+    putPixel(320-1, 0, 15);     // top right
+    putPixel(320-1, 200-1, 15); // bottom right
+    putPixel(0, 200-1, 15);     // bottom left
     waitForKey();
     
-    for(x; x < 50; x++) {
-        for(y; y < 50; y++) {
+    // Fill a 50x50 box with one color
+    short x;
+    short y;
+    for(x = 0; x < 50; x++) {
+        for(y = 0; y < 50; y++) {
             putPixel(x, y, 10);
         }
     }
-    
     waitForKey();
     
+    // Cycle through some different colors
     setPaletteRegister(10, 255, 255, 0);
     waitForKey();
+    setPaletteRegister(10, 0, 255, 255);
+    waitForKey();
+    setPaletteRegister(10, 128, 128, 255);
+    waitForKey();
+    setPaletteRegister(10, 255, 0, 0);
+    waitForKey();
+    setPaletteRegister(10, 0, 255, 0);
+    waitForKey();
+    setPaletteRegister(10, 0, 0, 255);
+    waitForKey();
     
+    // Set the video mode to 16-color 80x25 text
     setMode(0x03);
+    
+    // Return to the operating system
     return;
 }
 
