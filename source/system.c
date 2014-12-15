2a860fb5030b29c187a2699a62d685edb3bbed20
Blake Burgess <trinitek@live.com>
2014-12-15 18:49:53 -0500

    Add memory poke functions

--- source/system.c
+++ source/system.c
@@ -16,3 +16,39 @@ void outw(char port, short data) {
         "mov ax, [bp + 6]\n"
         "out dx, ax");
 }
+
+/*
+    Write a byte to the specified memory location
+    
+    @param
+        segment - destination memory segment
+        offset - destination memory offset
+        data - byte to write
+*/
+void mempokeb(short segment, short offset, char data) {
+    asm("push es\n"
+        "mov ax, [bp + 4]\n"
+        "mov es, ax\n"
+        "mov di, [bp + 6]\n"
+        "mov ax, [bp + 8]\n"
+        "stosb\n"
+        "pop es");
+}
+
+/*
+    Write a word to the specified memory location
+    
+    @param
+        segment - destination memory segment
+        offset - destination memory offset
+        data - word to write
+*/
+void mempokew(short segment, short offset, short data) {
+    asm("push es\n"
+        "mov ax, [bp + 4]\n"
+        "mov es, ax\n"
+        "mov di, [bp + 6]\n"
+        "mov ax, [bp + 8]\n"
+        "stosw\n"
+        "pop es");
+}
--- source/system.h
+++ source/system.h
@@ -1,3 +1,5 @@
 
 void outb(char port, char data);
 void outw(char port, short data);
+void mempokeb(short segment, short offset, char data);
+void mempokew(short segment, short offset, short data);
