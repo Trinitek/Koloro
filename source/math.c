fc3ae63263f14ca842e2fbf43a5f65cd3a7063ef
Blake Burgess <trinitek@live.com>
2014-12-16 22:55:45 -0500

    Add comments to math.c

--- source/math.c
+++ source/math.c
@@ -2,6 +2,15 @@
 #include "math.h"
 #include "fixed.h"
 
+/*
+    Calculate the sine of the given angle provided in radians
+    
+    @param
+        x - angle in radians
+    
+    @return
+        sine of the provided angle
+*/
 fixed sin(fixed x) {
     short divisor = 100;
     fixed result;
@@ -9,13 +18,32 @@ fixed sin(fixed x) {
         "fnclex \n"                 // clear previous exceptions
         
         "fild word [bp + 4] \n"     // ST0 = x
-        "fidiv word [bp + 6] \n"         // ST0 = ST0 / 100
+        "fidiv word [bp + 6] \n"    // ST0 = ST0 / 100
         "fsin \n"                   // ST0 = sin(ST0)
-        "fimul word [bp + 6] \n"         // ST0 = ST0 * 100
-        "fist word [bp + 8]"             // result = (int)ST0
+        "fimul word [bp + 6] \n"    // ST0 = ST0 * 100
+        "fist word [bp + 8]"        // result = (int)ST0
         );
 }
 
+/*
+    Calculate the cosine of the given angle provided in radians
+    
+    @param
+        x - angle in radians
+    
+    @return
+        cosine of the provided angle
+*/
 fixed cos(fixed x) {
-    //
+    short divisor = 100;
+    fixed result;
+    asm("fninit \n"                 // initialize FPU
+        "fnclex \n"                 // clear previous exceptions
+        
+        "fild word [bp + 4] \n"     // ST0 = x
+        "fidiv word [bp + 6] \n"    // ST0 = ST0 / 100
+        "fcos \n"                   // ST0 = cos(ST0)
+        "fimul word [bp + 6] \n"    // ST0 = ST0 * 100
+        "fist word [bp + 8]"        // result = (int)ST0
+        );
 }
