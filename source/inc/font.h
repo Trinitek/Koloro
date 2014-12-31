c3eb335c11931a26e8070de3fab801418d162c32
Blake Burgess <trinitek@live.com>
2014-12-30 21:02:19 -0500

    Populate character type enum

--- source/inc/font.h
+++ source/inc/font.h
@@ -3,7 +3,22 @@
 #define _FONT_H
 
 typedef enum {
-    N0, N1, N2, N3, N4, N5, N6, N7, N8, N9
+    // Numbers
+    N0, N1, N2, N3, N4, N5, N6, N7, N8, N9,
+    
+    // Symbols
+    AMPER, APOST, BRACEL, BRACER, BRACKETL, BRACKETR,
+    CARAT, CHEVL, CHEVR, COLON, COMMA, DASH, DOLLAR,
+    EXC, NUL, OCTO, PARENTHL, PARENTHR, PERCENT, PERIOD,
+    PIPE, PLUS, QUESTION, QUOTE, SEMI, SLASHB, SLASHF, UNDER,
+    
+    // Uppercase letters
+    UA, UB, UC, UD, UE, UF, UG, UH, UI, UJ, UK, UL, UM,
+    UN, UO, UP, UQ, UR, US, UT, UU, UV, UW, UX, UY, UZ,
+    
+    // Lowercase letters
+    LA, LB, LC, LD, LE, LF, LG, LH, LI, LJ, LK, LL, LM,
+    LN, LO, LP, LQ, LR, LS, LT, LU, LV, LW, LX, LY, LZ
 } chartype_t;
 
 #endif
